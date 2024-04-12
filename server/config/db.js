61eb559b5372e7b7ed8d03eb6066ac76d04d98b9
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2024-04-12 22:06:53 +0800

    connect database and add routes

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,14 @@
-// db here
+const mongoose = require('mongoose');
+const databaseURL = process.env.DATABASE_URL;
+
+const connectDb = async () => {
+    try {
+
+        await mongoose.connect(databaseURL);
+        console.log('Connection of Database: Success');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports =  connectDb;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+require('dotenv').config(); 
+const express = require('express');
+const app = express();
+const connectDb =  require('./config/db');
+
+// connecting to database
+connectDb();
+
+app.get('/AWS', (req, res) => {
+    res.status(200).json({message: "Hello, Guys!"});
+});
+
+app.listen(8080, () => {
+    console.log('Server is running in port 8080');
+})
